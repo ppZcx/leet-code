59c1b7b557acd33ec69d28cc152e27152f378b44
ppZcx <46839185+ppZcx@users.noreply.github.com>
2020-09-08 13:20:54 +0800

    提交信息

---
+++ leetCode/src/test/java/com/zcx/leetcode/LeetCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zcx.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
